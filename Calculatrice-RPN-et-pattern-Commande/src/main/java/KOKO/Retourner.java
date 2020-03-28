a374708598af81fda7aa74869392d15009684b71
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-28 22:48:03 +0100

    Create Retourner.java

---
+++ Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Retourner.java
@@ -0,0 +1,30 @@
+package KOKO;
+/**
+*classe Retourner.
+*utilise linterface command
+* @author Koussaila HAMMOUCHE.
+*/
+public class Retourner  implements Command {
+	/** MoteurRPNr.
+	    * @see Retourner#Retourner( moteurRPN )
+	    * @see Retourner#execute()
+	    */
+	private MoteurRPN moteurRPN;
+	/**
+	 * constructeur Retourner.
+	 * @param moteurRPN  
+	 * cest MoteurRPN
+	 * */
+	public Retourner(MoteurRPN moteurRPN){
+		this.moteurRPN=moteurRPN;
+	}
+	/**
+	 * methode execute.
+	 *  transmettre la demande à la méthode listeOperandes de la class moteurRPN.
+	 * */
+	public void execute() {
+		
+		this.moteurRPN.listeOperandes();
+	}
+
+}
