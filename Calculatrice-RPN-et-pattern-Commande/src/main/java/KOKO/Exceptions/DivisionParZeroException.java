f51444d8d06c6bd5aa6cb7d6129e7874a814dad3
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-28 22:47:33 +0100

    Create DivisionParZeroException.java

---
+++ Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Exceptions/DivisionParZeroException.java
@@ -0,0 +1,16 @@
+package KOKO.Exceptions;
+
+public class DivisionParZeroException extends Exception{
+	
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3637787669963394788L;
+
+	public DivisionParZeroException() {
+		super("denomitaur egal 0 ");
+	}
+
+}
