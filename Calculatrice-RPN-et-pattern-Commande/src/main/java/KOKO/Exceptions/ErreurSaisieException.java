d2d57e6deeaddd5a4103edee3b57e8aeb83c5db6
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-28 22:47:36 +0100

    Create ErreurSaisieException.java

---
+++ Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Exceptions/ErreurSaisieException.java
@@ -0,0 +1,17 @@
+package KOKO.Exceptions;
+
+public class ErreurSaisieException extends Exception {
+
+	
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7500440358432348737L;
+
+	public ErreurSaisieException() {
+		super("Erreur de saisie.");
+	}
+
+}
