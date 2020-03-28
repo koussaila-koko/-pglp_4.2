452243df84bbc677f5e11d4e86c7b8cebb011448
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-28 22:47:51 +0100

    Create Main.java

---
+++ Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Main.java
@@ -0,0 +1,43 @@
+package KOKO;
+
+
+
+	public class Main {
+
+	/*	public static void main(String[] args) {
+			try {
+
+				 String[] argss= {"u"};
+
+				// Creating the receiver object
+				MoteurRPN system = new MoteurRPN();
+				
+				// creating command and associating with receiver
+				Enregistrer e = new Enregistrer(system,55.0);
+				Appliquer l = new Appliquer(system,Operations.PLUS);
+
+
+				// Creating invoker and associating with Command
+				Invoker invoqueur = new Invoker();
+
+				// ajouter les commandes possible
+				invoqueur.addNewCommande("edit", e);
+				invoqueur.addNewCommande("e", e);
+				invoqueur.addNewCommande("list", l);
+				invoqueur.addNewCommande("ls", l);
+
+				// perform action on invoker object
+				
+					invoqueur.invoquer("edit");
+					invoqueur.invoquer("edit");
+					System.out.println(system.listeOperandes());
+					invoqueur.invoquer("ls");
+					System.out.println(system.listeOperandes());
+
+				
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}*/
+}
