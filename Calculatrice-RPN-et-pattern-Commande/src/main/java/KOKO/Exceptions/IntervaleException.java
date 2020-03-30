f3bc90ca48fc488dc1c36fc028a0da6b12d0a746
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-30 20:23:04 +0200

    Update IntervaleException.java

--- Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Exceptions/IntervaleException.java
+++ Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Exceptions/IntervaleException.java
@@ -1,16 +1,21 @@
+/**
+ *package des exceptions.
+ */
 package KOKO.Exceptions;
-
-
-		
-
+/**
+ * class IntervaleException.
+ * @see IntervaleException#IntervaleException()
+ */
 public class IntervaleException extends Exception {
-
-	/**
-	 * 
+/**
+	 *serialVersionUID.
 	 */
-	private static final long serialVersionUID = 7032173410409565137L;
-
+private static final long serialVersionUID = 7032173410409565137L;
+	/**
+	 *constructeur de la classe.
+	 */
 	public IntervaleException() {
 		super("opreandes non inclus dans l'intervalle ");
 	}
+
 }
