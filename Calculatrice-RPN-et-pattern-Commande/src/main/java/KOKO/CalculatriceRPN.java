66dfab33fb9186cb465018a6908564e176c71172
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-30 20:23:32 +0200

    Update CalculatriceRPN.java

--- Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/CalculatriceRPN.java
+++ Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/CalculatriceRPN.java
@@ -1,12 +1,28 @@
+/**
+ * package des classes,interfces et enum
+ */
 package KOKO;
-
+/**
+*enum CalculatriceRPN.
+* @author Koussaila_HAMMOUCHE.
+*/
 public enum CalculatriceRPN {
+	/**
+	 * le CALCULRPN.
+	 * */
 	CALCULRPN;
-	public void start( String[] args )
-    {
-		SaisieRPN Saisie = new SaisieRPN();
-		Saisie.saisie();
-    }
+	/**
+	 * methode start .
+	 *  @param args .
+	 * */
+	public void start(String[] args) {
+		SaisieRPN sa = new SaisieRPN();
+		sa.saisie();
+		}
+	/**
+	 * le main.
+	 * @param args .
+	 * */
 	public static void main(String[] args) {
 		CALCULRPN.start(args);
 	}
