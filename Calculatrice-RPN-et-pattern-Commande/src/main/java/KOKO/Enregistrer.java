03c57f317d489e564d3be85de2ac38aad1488db8
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-30 20:23:17 +0200

    Update Enregistrer.java

--- Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Enregistrer.java
+++ Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Enregistrer.java
@@ -5,7 +5,7 @@ package KOKO;
 * @author Koussaila HAMMOUCHE.
 */
 public class Enregistrer implements Command {
-	/** MoteurRPN .
+	/** MoteurRPN.
 	    * @see Appliquer#Appliquer(MoteurRPN,Operations)
 	    * @see Appliquer#execute()
 	    */
@@ -19,17 +19,18 @@ public class Enregistrer implements Command {
 	 * constructeur de la cEnregistrer .
 	 * @param moteurRPN
 	 *  c'est notre moteur.
-	 * @param val  
+	 * @param val
 	 * c'est la valeur a enregster dans la pile.
-	 * */	
-public Enregistrer(MoteurRPN moteurRPN,Double val){
-		this.moteurRPN=moteurRPN;
-		this.val=val;
-	}
+	 * */
+public Enregistrer(MoteurRPN moteurRPN, Double val) {
+		this.moteurRPN = moteurRPN;
+		this.val = val;
+		}
 /**
  * méthode execute.
  * la méthode pour openFile.
- * enregistrer la valeur grace a la methode enregistrerOperande de la classe moteurRPN
+ * enregistrer la valeur grace a la methode,
+ * enregistrerOperande de la classe moteurRPN
  * */
 public void execute() {
 		this.moteurRPN.enregistrerOperande(val);
