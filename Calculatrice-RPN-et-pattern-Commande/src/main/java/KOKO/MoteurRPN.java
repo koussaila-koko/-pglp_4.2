bb029f59876c0051b72829bfad7a739190aeb401
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-30 20:22:19 +0200

    Update MoteurRPN.java

--- Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/MoteurRPN.java
+++ Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/MoteurRPN.java
@@ -7,7 +7,6 @@ import java.util.Stack;
 *Appliquer une opération sur les opérandes,
 *Return lliste des Operandes dans la pile.
 * @author Koussaila HAMMOUCHE.
-*
 */
 public class MoteurRPN extends Interpreteur {
 	/**
@@ -19,48 +18,44 @@ public class MoteurRPN extends Interpreteur {
 	}
 	/**
 	 * méthode enregistrerOperande.
-	 * elle nous permet enregestrer l'operande et 
-	 * de grader la trace dans la operandesPrecedent 
+	 * elle nous permet enregestrer l'operande et
+	 * de grader la trace dans la operandesPrecedent
 	 * @param val opérande
 	 * */
-	// enregistrement d'une opérande
 	public void enregistrerOperande(double val) {
 		operandes.add(val);
-		Stack<Double> temp=new Stack<Double>();
+		Stack<Double> temp = new Stack<Double>();
 		temp.addAll(operandes);
 		operandesPrecedent.add(temp);
 	}
 	/**
 	 * méthode appliquerOperation.
-	 * @throws ArithmeticException 
-	 * elle nous permet d'effectuer une operation de calcule entre deux 
-	 * operande grace a la methode eval 
+	 * @throws ArithmeticException
+	 * elle nous permet d'effectuer une operation de calcule entre deux
+	 * operande grace a la methode eval
 	 * si il  manque d'operandes pour effectuer l'operation une exception
 	 * @param operation  operandes
 	 * */
-	public void appliquerOperation(Operations operation) throws ArithmeticException {
+	public void appliquerOperation(Operations operation)
+			throws ArithmeticException {
 		if (operandes.size() >= 2) {
 			double val1 = operandes.pop();
 			double val2 = operandes.pop();
 			operandes.add(operation.eval(val1, val2));
-			Stack<Double> temp=new Stack<Double>();
+			Stack<Double> temp = new Stack<Double>();
 			temp.addAll(operandes);
-			operandesPrecedent.add(temp);		
-			}
-		else
-			throw new ArithmeticException("vos operande (pas asez d'operandes pour effectuer l'operation)");
-	}
-
-	/**
+			operandesPrecedent.add(temp);
+			} else {
+				throw new ArithmeticException("pas assez d'operandes");
+				}
+		}
+   /**
 	 * methode listeOperandes.
 	 * elle permet d'retourner la liste des oprerandes stockées dans la pile
 	 * @return operandes de
 	 * la pile
 	 * */
-	public Stack <Double> listeOperandes() {
+	public Stack<Double> listeOperandes() {
 		return operandes;
-		
+		}
 	}
-
-
-}
