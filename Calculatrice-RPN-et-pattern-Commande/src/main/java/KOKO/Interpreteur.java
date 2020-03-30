7419679d8830d453241eb4e06041609ea0fad97c
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-30 20:22:22 +0200

    Update Interpreteur.java

--- Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Interpreteur.java
+++ Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Interpreteur.java
@@ -5,31 +5,28 @@ import java.util.Stack;
 *Implémentation d'un interpréteur on utilisant les pattern Commandes suivants:
 *undo supprime la dernière action.
 *quit pour quitter le programme.
-*la class est utilser pas les classes Undo Quit MoteurRPN 
+*la class est utilser pas les classes Undo Quit MoteurRPN
 * @author Koussaila HAMMOUCHE.
 */
-
-
 public class Interpreteur {
-	  /**
+    /**
      * Stack operandes les entiers pour empiler.
      * @see Interpreteur#Interpreteur()
      * @see Interpreteur#undo()
      */
 	protected Stack<Double> operandes;
 	/**
-     * Stack operandesPrecedent pour garder la trace de la dernier operation .
+     * Stack operandesPrecedent pour garder la trace de les operandes .
      * @see Interpreteur#Interpreteur()
      * @see Interpreteur#undo()
      */
 	protected Stack<Stack<Double>> operandesPrecedent;
-	
 	/**
 	 * constructeur Interpreteur.
 	 *operandes:
 	 *l'ensemble des operande aempiler
 	 *operandesPrecedent:
-	 * la liste qui contiens la trace des derniers operations 
+	 * la liste qui contiens la trace des derniers operations
 	 * */
 	public Interpreteur() {
 		operandes = new Stack<Double>();
@@ -41,25 +38,18 @@ public class Interpreteur {
 	 * */
 	public void quit() {
 		System.exit(0);
-		
-	}
+		}
 	/**
 	 * Methode undo.
 	 * supprime la dernière action.
 	 * */
 	public void undo() {
-		
-
-		if(operandesPrecedent.size()>1) {
+		if (operandesPrecedent.size() > 1) {
 			operandesPrecedent.pop();
-			operandes=operandesPrecedent.peek();
-		}else {
+			operandes = operandesPrecedent.peek();
+			} else {
 			operandesPrecedent.clear();
 			operandes.clear();
 		}
-		
-
 	}
-
-
 }
