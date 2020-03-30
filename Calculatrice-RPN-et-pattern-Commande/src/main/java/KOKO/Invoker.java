7becca2cc8b756e2c55226a92b44b09899f626e4
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-30 20:22:25 +0200

    Update Invoker.java

--- Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Invoker.java
+++ Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Invoker.java
@@ -1,38 +1,50 @@
 package KOKO;
-
 import java.util.HashMap;
 import java.util.Map;
-
-
+/**
+*classe Invoker.
+* @author Koussaila HAMMOUCHE.
+*/
 public class Invoker {
-
+	 /**
+	  * commandes
+      * Stack operandes les entiers pour empiler.
+      * @see Invoker#getCommandes()
+      * @see invoquer#addNewCommande(nomCommande,cmd)
+      * @see Invoker#invoquer(nomCommande)
+      */
 	private Map<String, Command> commandes = new HashMap<String, Command>();
-
+	 /**
+	  * Methode getCommandes
+      * Stack operandes les entiers pour empiler.
+      *@return commandes
+      */
 	public Map<String, Command> getCommandes() {
 		return commandes;
 	}
-
+	 /**
+	  * Methode addNewCommande.
+	  * @param nomCommande .
+	  * @param cmd .
+     */
 	public void addNewCommande(String nomCommande, Command cmd) {
 		commandes.put(nomCommande, cmd);
 	}
-	
-	 
+	 /**
+	  * constructeur avec nomCommande.
+	  * @param nomCommande .
+    */
 	public void invoquer(String nomCommande) {
 		Command cmd = commandes.get(nomCommande);
 		if (cmd != null) {
 			cmd.execute();
 		}
 	}
-	
+	 /**
+	  * constructeur avec Command.
+	  * @param cmd .
+   */
 	 public void invoquer(Command cmd) {
 	        cmd.execute();
 	    }
-	// c'est a verifier c'est n'est pas encore valider !!!!!!!!!!!!!!
-		/*public void affiche_pile() {
-			for(int i=0;i<commandes.size();i++) {
-				System.out.print(commandes.get(i) + " ");
-			}
-			System.out.print("\n");
-		}*/
-
-}
+	 }
