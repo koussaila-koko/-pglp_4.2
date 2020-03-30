01396c0249ea4f0f42a6aefdd3956f0bf5d6b8e9
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-30 20:23:12 +0200

    Update DivisionParZeroException.java

--- Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Exceptions/DivisionParZeroException.java
+++ Calculatrice-RPN-et-pattern-Commande/src/main/java/KOKO/Exceptions/DivisionParZeroException.java
@@ -1,14 +1,20 @@
+/**
+ *package des exceptions.
+ */
 package KOKO.Exceptions;
-
-public class DivisionParZeroException extends Exception{
-	
-	
-
+/**
+ * class DivisionParZeroException.
+ * c'est pour la division sur 0
+ * @see DivisionParZeroException#DivisionParZeroException()
+ */
+public class DivisionParZeroException extends Exception {
 	/**
-	 * 
+	 *serialVersionUID.
 	 */
 	private static final long serialVersionUID = 3637787669963394788L;
-
+	/**
+	 * constructeur de la classe.
+	 */
 	public DivisionParZeroException() {
 		super("denomitaur egal 0 ");
 	}
